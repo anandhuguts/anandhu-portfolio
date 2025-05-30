a4880d57c04ffb9bf2d11276cf866b433c78f546
Anandhu <anandhup059@gmail.com>
2025-05-30 15:44:23 +0530

    overview

--- src/components/pages/Overview.jsx
+++ src/components/pages/Overview.jsx
@@ -7,8 +7,8 @@ import Background3D from "../sub components/Background";
 function Overview() {
   return (
     <Element name="about">
-      <section className="relative flex justify-center w-full min-h-[766px] bg-[#0C1328] text-white overflow-hidden ">
-        <div>
+      <section className="relative flex  w-full min-h-[766px] bg-[#0C1328] text-white overflow-hidden ">
+        <div className=" mx-auto px-[50px]">
           <motion.div
             initial={{ y: -100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
@@ -18,20 +18,19 @@ function Overview() {
             <h2 className="font-script font-bold text-white text-[47px]">
               Overview
             </h2>
-            <p className=" font-script font-normal text-[#D8D8D8] text-[16px] leading-[28px] tracking-[1.2px] mt-[11px]">
+            <p className=" max-w-[750px] font-script font-normal text-[#D8D8D8] text-[16px] leading-[28px] tracking-[1.2px] mt-[11px]">
               As a creative MERN stack developer and web designer, I specialize
-              in crafting high- <br />
-              performance, visually stunning digital experiences. With a strong
-              passion for <br /> building dynamic web applications, I seamlessly
-              blend functionality with aesthetic <br /> design, ensuring every
-              project is both efficient and visually engaging. My expertise{" "}
-              <br /> lies in developing intuitive user experiences, combining
-              creativity with technical <br /> proficiency to deliver solutions
-              that are not only beautifully designed but also highly <br />{" "}
-              optimized for performance and usability.
+              in crafting high- performance, visually stunning digital
+              experiences. With a strong passion for building dynamic web
+              applications, I seamlessly blend functionality with aesthetic
+              design, ensuring every project is both efficient and visually
+              engaging. My expertise lies in developing intuitive user
+              experiences, combining creativity with technical proficiency to
+              deliver solutions that are not only beautifully designed but also
+              highly optimized for performance and usability.
             </p>
           </motion.div>
-          <div className="flex gap-[20px] mt-[50px]">
+          <div className="flex flex-wrap gap-[20px] mt-[50px]">
             {Service.map((service, index) => {
               return (
                 <Services
